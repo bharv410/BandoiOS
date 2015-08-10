b10111a6eb5520f923b6b2631db4053441e34cf7
bharv410 <benjamin.harvey@globant.com>
2015-08-10 10:43:59 -0700

    Initial Commit

---
+++ Bando/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Bando
+//
+//  Created by Benjamin Harvey on 8/10/15.
+//  Copyright (c) 2015 Benjamin Harvey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
